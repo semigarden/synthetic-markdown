ebe6b0bbea54f1e47dc21e239c7a0af50149c5c8
semigarden <semigardenn@gmail.com>
2025-12-22 14:07:50 +0400

    add db save/load

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from '@/styles/components/Editor.module.scss'
 import { parseBlock, renderBlock } from '@/utils/parser'
+import useStore from '@/hooks/useStore'
 
 const Editor: React.FC = () => {
+  const { loadText, saveText } = useStore()
   const [text, setText] = useState('')
+  const hasLoadedRef = useRef(false)
+
+  useEffect(() => {
+    let cancelled = false
+
+    loadText()
+      .then((value) => {
+        if (!cancelled) {
+          setText(value)
+          hasLoadedRef.current = true
+        }
+      })
+      .catch((error) => console.error(error))
+
+    return () => {
+      cancelled = true
+    }
+  }, [])
+
+  useEffect(() => {
+    if (hasLoadedRef.current) {
+      saveText(text).catch((error) => console.error(error))
+    }
+  }, [text])
 
   const document = parseBlock(text)
 
