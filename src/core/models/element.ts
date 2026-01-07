edc75879adb53e5f78b853d1687dc8a20812e85b
semigarden <semigardenn@gmail.com>
2026-01-07 15:35:17 +0400

    fix initial render

--- src/core/models/element.ts
+++ src/core/models/element.ts
@@ -29,13 +29,14 @@ class Element extends HTMLElement {
 
         this.addStyles()
         this.addDOM()
-        this.renderAST()
 
         this.render = new Render(this.rootElement!)
         this.caret = new Caret(this.rootElement!)
         this.editor = new Editor(this.ast, this.caret, this.render, this.emitChange.bind(this))
         this.selection = new Selection(this.ast, this.caret, this.rootElement!)
         this.selection.attach()
+
+        this.renderAST()
     }
 
     disconnectedCallback() {
